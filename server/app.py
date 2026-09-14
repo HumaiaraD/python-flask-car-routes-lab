02e0ca2bf5c9883e6b4418ed48de1cbe7f638a3a
humaiarad <d.humaiara@gmail.com>
2026-09-14 12:03:17 -0400

    Add Flask welcome and model routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,17 @@
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to Flatiron Cars!</h1>"
+
+@app.route('/<model>')
+def model_route(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog."
